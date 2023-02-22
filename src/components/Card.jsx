9c967c5d555dfaca2680038b41c2daa4428d8323
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-22 18:34:22 +0900

    Add styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,17 @@ import "./Card.css";
 const Card = ({ game }) => {
   return (
     <div className="Card">
+      <div className="Card__date">
+        <div>{game.date}</div>
+      </div>
       <div className="Card__title">
-        {game.player1} VS {game.player2}
+        {game.player1} {<br />}vs{<br />} {game.player2}
       </div>
       <div className="Card__content">
+        <div className="time">{game.time}</div>
+        <hr />
         <div>{game.stadium}</div>
-        <div>{game.location}</div>
+        <div className="location">{game.location}</div>
       </div>
       <button className="Card__button">Preview</button>
     </div>
