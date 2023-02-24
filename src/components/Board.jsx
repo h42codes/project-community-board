0fd64101b55b98d33287f2e3f5060670412053f4
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-24 11:03:50 +0900

    Add filtering feature

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import "./Board.css";
 import Card from "./Card";
+import SearchBar from "./SearchBar";
 
 const Board = ({ gameList }) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  let filteredGames = gameList.filter((game) => {
+    return (
+      game.player1.toLowerCase().includes(searchInput.toLowerCase()) ||
+      game.player2.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  });
+
+  filteredGames = searchInput ? filteredGames : gameList;
+
   return (
     <div className="Board">
-      {gameList.map((game) => (
-        <Card key={game.id} game={game} />
-      ))}
+      <div className="search-container">
+        <SearchBar handleChange={handleInputChange} value={searchInput} />
+      </div>
+      <div className="cards-container">
+        {filteredGames.map((game) => (
+          <Card key={game.id} game={game} />
+        ))}
+      </div>
     </div>
   );
 };
